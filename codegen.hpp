7c04e7da71529c5244c8eafa129fa8f49e3963e2
jp05451 <jp05451@yahoo.com.tw>
2024-06-12 05:39:49 +0800

    add code generation

--- codegen.hpp
+++ codegen.hpp
@@ -1,8 +1,164 @@
-#include"symbol.hpp"
+#include "symbol.hpp"
+#include <fstream>
 
 extern ofstream output;
 
-void print(symbol *s)
+enum op
 {
-    output<<
+    A_D_D,
+    S_U_B,
+    M_U_L,
+    D_I_V
+};
+
+void G_op(op oper, symbol *a, symbol *b)
+{
+    // check if is array
+    if (isArray(a) || isArray(b))
+        return;
+
+
+    if (a->isConst())
+    {
+        if (a->S_type == INT_TYPE)
+            output << a->S_data.int_data;
+        if (a->S_type == REAL_TYPE)
+            output << a->S_data.real_data;
+    }
+    else
+    {
+        if (a->S_type == INT_TYPE)
+            output << a->id;
+        if (a->S_type == REAL_TYPE)
+            output << a->id;
+    }
+
+    switch (oper)
+    {
+    case A_D_D:
+        output << " + ";
+        return;
+    case S_U_B:
+        output << " - ";
+        return;
+    case M_U_L:
+        output << " * ";
+        return;
+    case D_I_V:
+        output << " / ";
+        return;
+    }
+
+    if (b->isConst())
+    {
+        if (a->S_type == INT_TYPE)
+            output << b->S_data.int_data;
+        if (a->S_type == REAL_TYPE)
+            output << b->S_data.real_data;
+    }
+    else
+    {
+        if (a->S_type == INT_TYPE)
+            output << b->S_data.int_data;
+        if (a->S_type == REAL_TYPE)
+            output << b->S_data.real_data;
+        // if (a->S_type == INT_TYPE)
+        //     output << b->id;
+        // if (a->S_type == REAL_TYPE)
+        //     output << b->id;
+    }
+}
+
+
+
+void G_print(symbol *s)
+{
+    output << "printf(\"";
+    if (!isArray(s))
+    {
+        if (s->S_type == INT_TYPE)
+        {
+            output << s->S_data.int_data;
+        }
+        if (s->S_type == REAL_TYPE)
+        {
+            output << s->S_data.real_data;
+        }
+    }
+    else if (isArray(s))
+    {
+        if (s->S_type == INT_TYPE)
+            for (auto a : s->S_data.array_data)
+            {
+                output << a.int_data << ",";
+            }
+        if (s->S_type == REAL_TYPE)
+            for (auto a : s->S_data.array_data)
+            {
+                output << a.real_data << ",";
+            }
+    }
+    output << "\");" << endl;
+}
+
+void G_println(symbol *s)
+{
+    output << "printf(\"";
+    if (!isArray(s))
+    {
+        if (s->S_type == INT_TYPE)
+        {
+            output << s->S_data.int_data;
+        }
+        if (s->S_type == REAL_TYPE)
+        {
+            output << s->S_data.real_data;
+        }
+    }
+    else if (isArray(s))
+    {
+        if (s->S_type == INT_TYPE)
+            for (auto a : s->S_data.array_data)
+            {
+                output << a.int_data << ",";
+            }
+        if (s->S_type == REAL_TYPE)
+            for (auto a : s->S_data.array_data)
+            {
+                output << a.real_data << ",";
+            }
+    }
+    output << "\\n\");" << endl;
+}
+
+void G_variable(symbol *s)
+{
+    if (s->id == "" || s->S_flag == CONSTANT)
+    {
+        return;
+    }
+    if (s->S_type == INT_TYPE && !isArray(s))
+    {
+        output << "int " << s->id << " = " << s->S_data.int_data << ";" << endl;
+        return;
+    }
+    if (s->S_type == REAL_TYPE && !isArray(s))
+    {
+        output << "float " << s->id << " = " << s->S_data.real_data << ";" << endl;
+        return;
+    }
+}
+
+void G_main()
+{
+    output << "#include<stdio.h>" << endl
+           << endl;
+    output << "int main()" << endl;
+    output << "{" << endl;
+}
+
+// End the output file
+void G_end()
+{
+    output << "}" << endl;
 }
